a6b74b4d85c817de98da64c23e74cb0a1b008ed4
조성원 <tjddnjsjo@kakao.com>
2024-04-28 00:52:32 +0900

    refactor: 문구 수정

--- client/src/routes/register.jsx
+++ client/src/routes/register.jsx
@@ -49,7 +49,7 @@ function Register() {
             <Typography color="gray" className="text-center">
               이미 계정이 있으신가요?{' '}
               <a href="#" className="font-medium text-gray-900">
-                로그인하기
+                로그인
               </a>
             </Typography>
           </div>
