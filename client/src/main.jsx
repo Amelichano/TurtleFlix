316d8b0c43607f3619294847dfd024fbc34a575f
조성원 <tjddnjsjo@kakao.com>
2024-04-28 00:47:44 +0900

    style: import문 순서 정리

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from '@material-tailwind/react'
 
 import Root from './routes/root'
