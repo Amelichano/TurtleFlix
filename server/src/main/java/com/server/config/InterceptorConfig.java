eaecbfb352aa70e67ed75cd14ca7f40f1e6ae55f
JaeHoonSong <128021502+SongJaeHoonn@users.noreply.github.com>
2024-05-13 13:05:15 +0900

    fix: 접근권 변경

--- server/src/main/java/com/server/config/InterceptorConfig.java
+++ server/src/main/java/com/server/config/InterceptorConfig.java
@@ -2,8 +2,8 @@ package com.server.config;
 
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+        import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+        import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
