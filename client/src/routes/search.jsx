c1981ce0684ba5ba9159ea3f920858826ff43b28
조성원 <tjddnjsjo@kakao.com>
2024-05-13 22:52:25 +0900

    test: api 연결 테스트용 request

--- client/src/routes/search.jsx
+++ client/src/routes/search.jsx
@@ -1,8 +1,16 @@
+import { useEffect } from 'react'
 import MovieCard from '../components/movie-card'
 
 import { MOVIE_DATA } from '../data/movie'
+import { getSearch } from '../api/search'
 
 function Search() {
+  useEffect(() => {
+    const result = getSearch('horror', 's')
+
+    console.log(result)
+  }, [])
+
   return (
     <div className="grid grid-cols-1 justify-items-center gap-8 md:grid-cols-2 lg:grid-cols-3">
       {MOVIE_DATA.map((movie) => (
