aa44d740082ddceb927e7e32b5035dbdf8330880
조성원 <tjddnjsjo@kakao.com>
2024-05-15 00:43:10 +0900

    feat: 세로 가운데 정렬

--- client/src/layouts/base-layout.jsx
+++ client/src/layouts/base-layout.jsx
@@ -5,7 +5,7 @@ function BaseLayout({ children }) {
   return (
     <div className="relative mx-auto flex min-h-dvh max-w-screen-xl flex-col p-4">
       <NavMenu />
-      <main className="mb-16 flex-1 py-6">{children}</main>
+      <main className="mb-16 flex flex-1 items-center py-6">{children}</main>
       <Footer />
     </div>
   )
