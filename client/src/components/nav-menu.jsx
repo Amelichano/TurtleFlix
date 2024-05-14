f139a92de7cd3e9598ba8a20aed10c8f310b86a5
조성원 <tjddnjsjo@kakao.com>
2024-05-14 20:13:34 +0900

    feat: nav-menu logo 적용

--- client/src/components/nav-menu.jsx
+++ client/src/components/nav-menu.jsx
@@ -8,6 +8,9 @@ import {
   ListItem,
 } from '@material-tailwind/react'
 import { Cross1Icon, HamburgerMenuIcon } from '@radix-ui/react-icons'
+import { Link } from 'react-router-dom'
+
+import Logo from '../assets/logo.svg'
 
 function NavList() {
   return (
@@ -51,14 +54,9 @@ function NavMenu() {
   return (
     <Navbar className="mx-auto max-w-screen-xl px-4 py-2">
       <div className="flex items-center justify-between text-blue-gray-900">
-        <Typography
-          as="a"
-          href="#"
-          variant="h6"
-          className="mr-4 cursor-pointer py-1.5 lg:ml-2"
-        >
-          Material Tailwind
-        </Typography>
+        <Link href="/" className="mr-4 cursor-pointer py-1.5 lg:ml-2">
+          <img src={Logo} alt="Logo" className="h-8" />
+        </Link>
         <div className="hidden lg:block">
           <NavList />
         </div>
