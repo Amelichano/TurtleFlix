866412c0c773e2a7a94939187d2b2c905af036ac
조성원 <tjddnjsjo@kakao.com>
2024-05-14 20:52:19 +0900

    feat: 페이지네이션 구현

--- client/src/api/search.js
+++ client/src/api/search.js
@@ -1,9 +1,10 @@
 import { serverInstance } from './instance'
 
-const getSearch = async (genre, title) => {
+const getSearch = async (genre, title, page) => {
   const genreQuery = genre && `genreName=${genre}`
   const titleQuery = title && `title=${title}`
-  const query = `?${[genreQuery, titleQuery].filter(Boolean).join('&')}`
+  const pageQuery = page && `page=${page}`
+  const query = `?${[genreQuery, titleQuery, pageQuery].filter(Boolean).join('&')}`
 
   const response = await serverInstance.get(`/api/search${query}`)
   return response.data
