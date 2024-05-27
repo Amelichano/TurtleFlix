8b4b439599da77987fd55fd47aa496d6b71f27a2
Hojin Han <amelichano@naver.com>
2024-05-27 23:27:08 +0900

    fix: BaseLayout이 적용되지 않은 문제 수정

--- client/src/routes/testMovieDetails.jsx
+++ client/src/routes/testMovieDetails.jsx
@@ -1,4 +1,5 @@
 import MovieDetails from '../components/movieDetails';
+import BaseLayout from '../layouts/base-layout'
 
 function TestMovieDetails({ tmdbId }) {
   const testTmdbId = '617127'; // 임의로 설정한 tmdbId
