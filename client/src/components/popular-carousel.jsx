cc5f437178d7e462fbb45bd7575dcbb7e840c06e
조성원 <tjddnjsjo@kakao.com>
2024-05-29 08:29:16 +0900

    feat: tmdb의 puplar movies를 불러와 정보를 표시하는 캐러셀 제작

---
+++ client/src/components/popular-carousel.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react'
+import { Carousel } from '@material-tailwind/react'
+import { Link } from 'react-router-dom'
+
+import { getPopular } from '../api/movies'
+
+function PopularCarousel() {
+  const [popular, setPopular] = useState([])
+
+  useEffect(() => {
+    async function getPopularMovies() {
+      const popularMovies = await getPopular()
+      setPopular(popularMovies.results.slice(0, 10))
+      console.log(popularMovies.results.slice(0, 10))
+    }
+
+    getPopularMovies()
+  }, [])
+
+  return (
+    <Carousel className="w-full rounded-xl">
+      {popular.map((movie) => (
+        <Link to={`/details/${movie.id}`} className="group relative">
+          <img
+            src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`}
+            className="h-80 w-full object-cover brightness-50 transition group-hover:brightness-[.35] md:aspect-video md:h-full"
+          />
+          <div className="absolute bottom-0 left-0 p-16">
+            <h3 className="mb-4 line-clamp-1 text-4xl font-bold text-white">
+              {movie.title}
+            </h3>
+            <p className="line-clamp-4 w-full text-white md:w-1/2">
+              {movie.overview}
+            </p>
+          </div>
+        </Link>
+      ))}
+    </Carousel>
+  )
+}
+
+export default PopularCarousel
