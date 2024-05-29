3f92aebc4a4e3e39c34419732ada6a3609f816d7
조성원 <tjddnjsjo@kakao.com>
2024-05-29 15:19:13 +0900

    feat: 좋아하는 영화 및 추천 영화 목록 화면 표시

--- client/src/components/liked-movies.jsx
+++ client/src/components/liked-movies.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { Typography } from '@material-tailwind/react'
 
 import { getLikedMovies } from '../api/movies'
+import MoviesCarousel from './movies-carousel'
 
 function LikedMovies() {
   const [liked, setLiked] = useState([])
@@ -20,7 +21,7 @@ function LikedMovies() {
       좋아하는 영화가 없습니다.
     </Typography>
   ) : (
-    <></>
+    <MoviesCarousel movies={liked} />
   )
 }
 
--- client/src/components/liked-recommend.jsx
+++ client/src/components/liked-recommend.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { Typography } from '@material-tailwind/react'
 
 import { getLikedRecommendations } from '../api/movies'
+import MoviesCarousel from './movies-carousel'
 
 function LikecRecommend() {
   const [recommendations, setRecommendations] = useState([])
@@ -20,7 +21,7 @@ function LikecRecommend() {
       좋아요를 눌러서 영화를 추천받아보세요!
     </Typography>
   ) : (
-    <></>
+    <MoviesCarousel movies={recommendations} />
   )
 }
 
