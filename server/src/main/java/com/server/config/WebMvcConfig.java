8625afb5cb5954502dabe4e22f4e92caab3c9a11
JaeHoonSong <128021502+SongJaeHoonn@users.noreply.github.com>
2024-05-29 13:44:25 +0900

    fix: CORS 재설정

--- server/src/main/java/com/server/config/WebMvcConfig.java
+++ server/src/main/java/com/server/config/WebMvcConfig.java
@@ -9,8 +9,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowCredentials(true);
     }
 }
